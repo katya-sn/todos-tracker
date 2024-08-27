c9257392fddcf37528d2201a8af8f43dde6b953f
katya-sn <kattya.sagun@gmail.com>
2024-08-27 17:05:23 +0300

    todos tracker clone

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-import './App.scss';
-
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
+import { GlobalProvider } from './context/TodoContext';
+import { TodoApp } from './components/TodoApp';
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <GlobalProvider>
+      <TodoApp />
+    </GlobalProvider>
   );
 };
